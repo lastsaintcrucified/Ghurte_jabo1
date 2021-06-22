3f042705e786c3349fab9ff0bd7300dae019831e
lastsaintcrucified <lastsaintcrucified@gmail.com>
2021-06-22 16:46:59 +0600

    http hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,49 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState();
+  const activeHttpRequest = useRef([]);
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setLoading(true);
+      console.log("hello")
+      const httpAbortCrtl = new AbortController();
+      activeHttpRequest.current.push(httpAbortCrtl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCrtl.signal,
+        });
+        
+        const data = await response.json();
+        activeHttpRequest.current = activeHttpRequest.current.filter(
+            (abortCrtl) => abortCrtl !== httpAbortCrtl
+          );
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+        setLoading(false);
+        return data;
+      } catch (error) {
+        setLoading(false);
+        setErrMsg(error.message);
+        throw error;
+      }
+    },
+    []
+  );
+
+  const errorHandler = () => {
+    setErrMsg(null);
+  };
+  useEffect(() => {
+    return () => {
+      activeHttpRequest.current.forEach((abortCrtl) => abortCrtl.abort());
+    };
+  }, []);
+  return { isLoading, errMsg, sendRequest, errorHandler };
+};
--- src/users/pages/users.page.jsx
+++ src/users/pages/users.page.jsx
@@ -1,42 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook.js";
 import LoadingSpinner from "../../shared/uiElements/LoadingSpinner.jsx";
 import ErrorModal from "../../shared/uiElements/ErrorModal.jsx";
 import UserList from "../component/userList/userList.component.jsx";
 import "./users.styles.css";
 
 const User = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [errMsg, setErrMsg] = useState();
+  const { isLoading, errMsg, sendRequest, errorHandler } = useHttpClient();
   const [loadedUser, setLoadedUser] = useState();
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const fetchUser = async () =>{
       try {
-        const response = await fetch("http://localhost:5000/api/users");
-        const data = await response.json();
-        if(!response.ok){
-            throw new Error(data.message);
-        }
+        const data = await sendRequest("http://localhost:5000/api/users");
+  
         setLoadedUser(data.users);
-        
-      } catch (err) {
-        setErrMsg(err.message);
-      }
-      setIsLoading(false);
-    };
-    sendRequest();
-  }, []);
-  const errorHandler = () => {
-    setErrMsg(null);
-  };
+      } catch (err) {}
+    }
+    fetchUser();
+  }, [sendRequest]);
+ 
   return (
     <React.Fragment>
       <ErrorModal error={errMsg} onClear={errorHandler} />
+      <div className="center">{isLoading && <LoadingSpinner />}</div>
       <div className="center">
-      {isLoading && <LoadingSpinner/>}
-      </div>
-      <div className="center">
-        {loadedUser?<UserList items={loadedUser} />:<LoadingSpinner/>}
+        {loadedUser ? <UserList items={loadedUser} /> : <LoadingSpinner />}
       </div>
     </React.Fragment>
   );
